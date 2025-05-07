0df9986a0984d2672c23dd3211f06715a6bc13d4
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2025-05-07 02:33:03 +0100

    chore: more cleaning

--- enderio-base/src/main/java/com/enderio/base/client/EnderIOBaseClient.java
+++ enderio-base/src/main/java/com/enderio/base/client/EnderIOBaseClient.java
@@ -2,6 +2,7 @@ package com.enderio.base.client;
 
 import com.enderio.EnderIOBase;
 import com.enderio.base.api.EnderIO;
+import com.enderio.base.api.soul.SoulBoundUtils;
 import com.enderio.base.client.decorator.GlassIconDecorator;
 import com.enderio.base.client.paint.model.PaintedBlockGeometry;
 import com.enderio.base.client.particle.RangeParticle;
@@ -58,7 +59,7 @@ public class EnderIOBaseClient {
 
         event.enqueueWork(() -> {
             ItemProperties.register(EIOItems.SOUL_VIAL.get(), SoulVialItem.FILLED_MODEL_PROPERTY,
-                    (stack, level, player, seed) -> SoulVialItem.isFilled(stack) ? 1 : 0);
+                    (stack, level, player, seed) -> SoulBoundUtils.isBound(stack) ? 1 : 0);
         });
     }
 
